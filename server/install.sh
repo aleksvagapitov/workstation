07ec224a204719a6e73bcdd693860be314ee266e
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2024-01-12 02:54:24 +0000

    Changing config

--- server/install.sh
+++ server/install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]; then
-    echo "You need to run this script as root"
-    exit 1
-fi
 
-apt install -y software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install -y ansible
+sudo apt install -y software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
 
-if (( "${?}" != 0 )); then
-   echo "Updates were not successful."
-   exit 1
-fi
+SERVER_INSTALL_START=$(date "+%s")
+sudo ansible-playbook local.yml
+SERVER_INSTALL_END=$(date "+%s") 
+
+DOTFILES_INSTALL_START=$(date "+%s")
+cd ~/.dotfiles/ansible && ansible-playbook -e "ansible_become_password=user" local.yml 
+DOTFILES_INSTALL_END=$(date "+%s")
+
+echo "Server Install time: " $((SERVER_INSTALL_END-SERVER_INSTALL_START))
+echo "Dotfiles Install time: " $((DOTFILES_INSTALL_END-DOTFILES_INSTALL_START))
 
 exit 0
