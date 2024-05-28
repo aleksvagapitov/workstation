1faa6924146ef26f0136d3e3eeeaec379ed57d8b
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2024-05-28 00:47:04 +0000

    Changing to nyc

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ provider "digitalocean" {
 resource "digitalocean_droplet" "workstation" {
   image       = "ubuntu-22-04-x64"
   name        = "workstation"
-  region      = "sgp1"
-  size        = "s-1vcpu-2gb"
+  region      = "nyc1"
+  size        = "s-1vcpu-1gb"
   ssh_keys    = [digitalocean_ssh_key.workstation.id]
 
   provisioner "remote-exec" {
